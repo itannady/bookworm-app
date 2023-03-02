00c150f7985bc8845d93f4838b37e6e1700b0e3b
Isabella Tannady <isabellatannady@gmail.com>
2023-03-02 12:26:08 -0800

    feat: login and signup files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HeroComponent } from './hero/hero.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, HeroComponent],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HeroComponent,
+    SignupComponent,
+    LoginComponent,
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],

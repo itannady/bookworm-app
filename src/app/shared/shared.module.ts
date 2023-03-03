2e7529e8123998006d4b8cc00c71823ebed1aee7
Isabella Tannady <isabellatannady@gmail.com>
2023-03-02 20:59:05 -0800

    style(card): update book card

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { BookCardComponent } from './book-card/book-card.component';
 import { BooksCarouselComponent } from './books-carousel/books-carousel.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [NavbarComponent, BooksCarouselComponent],
+  declarations: [NavbarComponent, BooksCarouselComponent, BookCardComponent],
   imports: [CommonModule, RouterModule],
-  exports: [NavbarComponent],
+  exports: [NavbarComponent, BooksCarouselComponent, BookCardComponent],
 })
 export class SharedModule {}

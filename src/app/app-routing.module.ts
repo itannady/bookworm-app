0825acb19f0a953dace963a312a1a7981434ca67
Isabella Tannady <isabellatannady@gmail.com>
2023-03-05 12:38:45 -0800

    feat(signup): add form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
 import { BookListComponent } from './books/book-list/book-list.component';
 import { UserBooksComponent } from './books/user-books/user-books.component';
 import { HomeComponent } from './home/home.component';
@@ -9,6 +10,7 @@ const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'list', component: UserBooksComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
 ];
 
 @NgModule({

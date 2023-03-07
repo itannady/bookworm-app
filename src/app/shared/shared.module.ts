0cf085f02126a9dadda512f6da2a347c1841ef1b
Isabella Tannady <isabellatannady@gmail.com>
2023-03-07 14:31:37 -0800

    feat(loader): add loader component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { LoaderComponent } from './loader/loader.component';
 
 import { NavbarComponent } from './navbar/navbar.component';
 import { TruncatePipe } from './pipes/truncate.pipe';
 
 @NgModule({
-  declarations: [NavbarComponent, TruncatePipe],
+  declarations: [NavbarComponent, TruncatePipe, LoaderComponent],
   imports: [CommonModule, RouterModule],
-  exports: [NavbarComponent, TruncatePipe],
+  exports: [NavbarComponent, TruncatePipe, LoaderComponent],
 })
 export class SharedModule {}

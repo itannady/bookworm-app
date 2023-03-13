d6c05d4b04bf2a20cf6903ea1ecd1fbf150fa6e8
Isabella Tannady <isabellatannady@gmail.com>
2023-03-13 12:53:53 -0700

    feat(home): add auth

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   selectedBook: any;
+  userIsAuthenticated = false;
+  private authStatusSub: Subscription = new Subscription();
 
-  constructor() {}
+  constructor(private authService: AuthService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.userIsAuthenticated = this.authService.getIsAuth();
+    this.authStatusSub = this.authService
+      .getAuthStatusListener()
+      .subscribe((isAuthenticated) => {
+        this.userIsAuthenticated = isAuthenticated;
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.authStatusSub.unsubscribe();
+  }
 }

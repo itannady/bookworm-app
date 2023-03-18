898665ba24a7a26a72b5f0d31fb40b5d72a3f3d9
Isabella Tannady <isabellatannady@gmail.com>
2023-03-17 17:07:43 -0700

    style(footer): position to stay at bottom

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  currentPage: string = '';
+  constructor(private router: Router) {
+    this.router.events.subscribe((val) => {
+      this.currentPage = this.router.url;
+    });
   }
 
+  ngOnInit(): void {}
 }

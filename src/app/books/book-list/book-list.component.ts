cdf1919bd6a7ae589f0f6588a9fdabf206124c98
Isabella Tannady <isabellatannady@gmail.com>
2023-03-20 18:10:50 -0700

    fix(search): move to home page

--- src/app/books/book-list/book-list.component.ts
+++ src/app/books/book-list/book-list.component.ts
@@ -11,13 +11,15 @@ export class BookListComponent implements OnInit {
   currentGroup = 0;
   books: Book[] = [];
   @Output() bookSelected = new EventEmitter<Book>();
-
+  @Output() close = new EventEmitter<void>();
+  @Input() searchQuery: string = '';
   constructor(private booksService: BooksService) {}
 
   ngOnInit(): void {
     this.booksService.getBooksListener().subscribe((books) => {
       this.books = books;
     });
+    // Call your search method here with the updated query
   }
 
   prevClick() {
@@ -35,5 +37,10 @@ export class BookListComponent implements OnInit {
 
   onBookClick(book: Book) {
     this.bookSelected.emit(book);
+  }
+
+  onCloseClick() {
+    document.body.classList.remove('modalOpen'); // remove the CSS class from the body
+    this.close.emit();
   }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,14 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { Subscription } from 'rxjs';
 import { AuthService } from '../auth/auth.service';
+import { BooksService } from '../books/books.service';
 
 @Component({
   selector: 'app-home',
@@ -9,10 +17,20 @@ import { AuthService } from '../auth/auth.service';
 })
 export class HomeComponent implements OnInit, OnDestroy {
   selectedBook: any;
+  form: FormGroup;
+  showSearchResults = false;
   userIsAuthenticated = false;
+  @Output() searchQuery = new EventEmitter<string>();
   private authStatusSub: Subscription = new Subscription();
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    private booksService: BooksService,
+    private authService: AuthService
+  ) {
+    this.form = new FormGroup({
+      searchbar: new FormControl(''),
+    });
+  }
 
   ngOnInit(): void {
     this.userIsAuthenticated = this.authService.getIsAuth();
@@ -21,6 +39,14 @@ export class HomeComponent implements OnInit, OnDestroy {
       .subscribe((isAuthenticated) => {
         this.userIsAuthenticated = isAuthenticated;
       });
+  }
+
+  onSearch() {
+    const query = this.form.get('searchbar')?.value;
+    this.booksService.populateBooks(query);
+    this.showSearchResults = true;
+    console.log(this.form.get('searchbar')?.value);
+    this.searchQuery.emit(query);
   }
 
   ngOnDestroy(): void {

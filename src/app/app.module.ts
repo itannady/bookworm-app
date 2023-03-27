97b1535b5df7fac390647a044c4486ef6967a659
Isabella Tannady <isabellatannady@gmail.com>
2023-03-27 15:21:40 -0700

    Create separate module for angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { HeroComponent } from './hero/hero.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { LoginComponent } from './auth/login/login.component';
+import { HomeComponent } from './home/home.component';
+import { LibraryPageComponent } from './library-page/library-page.component';
+import { HeroComponent } from './hero/hero.component';
 import { BookListComponent } from './books/book-list/book-list.component';
 import { UserBooksComponent } from './books/user-books/user-books.component';
-import { AppRoutingModule } from './app-routing.module';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { SharedModule } from './shared/shared.module';
-import { HomeComponent } from './home/home.component';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { MatIconModule } from '@angular/material/icon';
-import { BookModalComponent } from './books/book-modal/book-modal.component';
-import { LibraryPageComponent } from './library-page/library-page.component';
-import { NotesComponent } from './notes/notes.component';
-import { AuthInterceptor } from './auth/auth-interceptor';
-
-import { MatInputModule } from '@angular/material/input';
-import { MatTooltipModule } from '@angular/material/tooltip';
 import { BestsellerListComponent } from './books/bestseller-list/bestseller-list.component';
 import { CategoryListComponent } from './books/category-list/category-list.component';
+import { BookModalComponent } from './books/book-modal/book-modal.component';
+import { NotesComponent } from './notes/notes.component';
+
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './shared/shared.module';
+import { AngularMaterialModule } from './angular-material.module';
+
+import { AuthInterceptor } from './auth/auth-interceptor';
 import { ErrorInterceptor } from './error-interceptor';
 
 @NgModule({
@@ -48,9 +47,7 @@ import { ErrorInterceptor } from './error-interceptor';
     SharedModule,
     HttpClientModule,
     NoopAnimationsModule,
-    MatIconModule,
-    MatInputModule,
-    MatTooltipModule,
+    AngularMaterialModule,
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
